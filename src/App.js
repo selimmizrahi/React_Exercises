793827e53bacdf834d6f2a691844d1715e889376
selimmizrahi <mizrahiselim@gmail.com>
2020-04-28 15:11:51 +0300

    Day 2 Exercise 3

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from "react";
+import Checkbox from "./Day2/checkbox";
 import Alive from "./Day2/alive";
 
 function App() {
-  return <Alive></Alive>;
+  return (
+    <Checkbox>
+      <Alive></Alive>
+    </Checkbox>
+  );
 }
 
 export default App;
