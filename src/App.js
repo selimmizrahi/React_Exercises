0f0c047da52696268a3e4da902f80e2abd095307
selimmizrahi <mizrahiselim@gmail.com>
2020-04-28 23:26:06 +0300

    Day 1 Geekout -please check in Galaxy phone mode

--- src/App.js
+++ src/App.js
@@ -1,12 +1,52 @@
 import React from "react";
-import Checkbox from "./Day2/checkbox";
-import Alive from "./Day2/alive";
+import Forecast from "./Day1/Geekout/Forecast";
+import Todayweather from "./Day1/Geekout/Todayweather";
+import sunpicture from "./Day1/Geekout/sunPicture.png";
+import "./Day1/App.css";
 
 function App() {
   return (
-    <Checkbox>
-      <Alive></Alive>
-    </Checkbox>
+    <div className="fullPage">
+      <div className="topPart">
+        <Todayweather
+          country={"Serbia"}
+          city={"Belgrade"}
+          weather={"19°C"}
+        ></Todayweather>
+      </div>
+      <div className="bottomPart">
+        <Forecast
+          day={"mon"}
+          date={"16.02"}
+          image={sunpicture}
+          weather={"Sunny"}
+          weatherDegree={"19°C"}
+          windSpeed={"4mph"}
+          humidity={"63%"}
+          cloudness={"14%"}
+        ></Forecast>
+        <Forecast
+          day={"tu"}
+          date={"17.02"}
+          image={sunpicture}
+          weather={"Rain"}
+          weatherDegree={"19°C"}
+          windSpeed={"4mph"}
+          humidity={"63%"}
+          cloudness={"14%"}
+        ></Forecast>
+        <Forecast
+          day={"we"}
+          date={"18.02"}
+          image={sunpicture}
+          weather={"Cloudy"}
+          weatherDegree={"19°C"}
+          windSpeed={"4mph"}
+          humidity={"63%"}
+          cloudness={"14%"}
+        ></Forecast>
+      </div>
+    </div>
   );
 }
 
