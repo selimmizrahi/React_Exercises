9dec553fec0b3040f14b170ff59cba1cc1952c7f
selimmizrahi <mizrahiselim@gmail.com>
2020-04-29 21:36:19 +0300

    Day 2 Geekout

--- src/App.js
+++ src/App.js
@@ -1,53 +1,13 @@
 import React from "react";
-import Forecast from "./Day1/Geekout/Forecast";
-import Todayweather from "./Day1/Geekout/Todayweather";
-import sunpicture from "./Day1/Geekout/sunPicture.png";
-import "./Day1/App.css";
+import "./Day2/Geekout/App.css"
+import Chat from "./Day2/Geekout/messageList";
+// import Form from './day2/Ex4.Geekout/form'
 
 function App() {
   return (
-    <div className="fullPage">
-      <div className="topPart">
-        <Todayweather
-          country={"Serbia"}
-          city={"Belgrade"}
-          weather={"19°C"}
-        ></Todayweather>
-      </div>
-      <div className="bottomPart">
-        <Forecast
-          day={"mon"}
-          date={"16.02"}
-          image={sunpicture}
-          weather={"Sunny"}
-          weatherDegree={"19°C"}
-          windSpeed={"4mph"}
-          humidity={"63%"}
-          cloudness={"14%"}
-        ></Forecast>
-        <Forecast
-          day={"tu"}
-          date={"17.02"}
-          image={sunpicture}
-          weather={"Rain"}
-          weatherDegree={"19°C"}
-          windSpeed={"4mph"}
-          humidity={"63%"}
-          cloudness={"14%"}
-        ></Forecast>
-        <Forecast
-          day={"we"}
-          date={"18.02"}
-          image={sunpicture}
-          weather={"Cloudy"}
-          weatherDegree={"19°C"}
-          windSpeed={"4mph"}
-          humidity={"63%"}
-          cloudness={"14%"}
-        ></Forecast>
-      </div>
+    <div className="main">
+      <Chat></Chat>
     </div>
   );
 }
-
 export default App;
